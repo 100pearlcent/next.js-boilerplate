040f2161f4db8260505f3ead714593473d9f8ff8
100pearlcent <100pearlcent@naver.com>
2022-11-25 13:53:43 +0900

    💾 Colors

---
+++ src/constants/Colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  primary: {
+    black: '#00000',
+    white: '#fffff',
+  },
+  secondary: {
+    gray: {
+      dark: '#303030',
+      medium: '#5E5E5E',
+      light: '#919191',
+      pale: '#C6C6C6',
+    },
+  },
+  accent: {
+    forest: '#254D4C',
+    ocean: '#00ADAD',
+    love: '#ED5E93',
+  },
+}
