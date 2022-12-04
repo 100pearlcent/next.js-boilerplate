a126ba45f317d3ccda66c1d3c78cccf6477a8481
100pearlcent <100pearlcent@naver.com>
2022-12-04 12:19:52 +0900

    🧹 Fix Typo

--- src/utils/Formatter.ts
+++ src/utils/Formatter.ts
@@ -1,7 +1,7 @@
-// ✨ 1,100 -> 1k
+// ✨ 1,100 -> 1.1K
 export const numberFormatter = (n: number) => {
   if (n < 1e3) return n
-  if (n >= 1e3) return +(n / 1e3).toFixed(1) + 'k'
+  if (n >= 1e3) return +(n / 1e3).toFixed(1) + 'K'
 }
 
 // ✨ 10000 -> 10,000
