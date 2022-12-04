89d29648e3eab7f5b0402ce7ddda0a530b371c12
100pearlcent <100pearlcent@naver.com>
2022-12-04 12:06:44 +0900

    🖼️ Reset CSS

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#000' }}>
+      <Link href="/">Header</Link>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout/Layout'
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
