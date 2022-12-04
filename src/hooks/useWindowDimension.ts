b1e7d47b2eb574549b764178c05bb30345aeed4d
100pearlcent <100pearlcent@naver.com>
2022-12-04 12:32:10 +0900

    ⚙️ useWindowDimension

---
+++ src/hooks/useWindowDimension.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+type WindowDimentions = {
+  width: number | undefined
+  height: number | undefined
+}
+
+const useWindowDimensions = (): WindowDimentions => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
+    width: undefined,
+    height: undefined,
+  })
+  useEffect(() => {
+    function handleResize(): void {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return (): void => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowDimensions
+}
+
+export default useWindowDimensions
+
+/*
+  ℹ️ Usage Example
+  import useWindowDimension from '@/hooks/useWindowDimension'
+  const { width, height } = useWindowDimensions();
+ */
