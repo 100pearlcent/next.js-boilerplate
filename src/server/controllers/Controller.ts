aeff977b6e7575872ff73b5deaa61544cdd28ca1
100pearlcent <100pearlcent@naver.com>
2022-12-15 10:55:32 +0900

    ⚙️ Server Controller

---
+++ src/server/controllers/Controller.ts
@@ -0,0 +1,19 @@
+import type { NextApiResponse } from 'next'
+import type { HttpResponse } from '@/server/interfaces/Http'
+import HttpError from '@/server/libs/HttpError'
+
+abstract class Controller {
+  protected sendJSON<T>(res: NextApiResponse, data: HttpResponse<T>) {
+    return res.status(data.code).send(data)
+  }
+
+  protected setError<E = string[]>(code: number, errors: E, message?: string) {
+    throw new HttpError(code, errors, message ?? 'HTTP errors')
+  }
+
+  protected handleError(res: NextApiResponse, error: Error | unknown) {
+    return HttpError.handle(res, error)
+  }
+}
+
+export default Controller
