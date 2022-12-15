43a996566843d0c8cc8015044a4c92dc6522ea3c
100pearlcent <100pearlcent@naver.com>
2022-12-15 10:28:01 +0900

    ⚙️ isCSR

--- src/utils/Indicator.ts
+++ src/utils/Indicator.ts
@@ -1,6 +1,8 @@
 // ✨ SSR 판단
 export const isSSR = typeof window === 'undefined'
 
+// ✨ CSR 판단
+export const isCSR = typeof window !== 'undefined'
+
 // ✨ DOM 판단
-export const isDOM =
-  !isSSR && window.document && window.document.documentElement
+export const isDOM = isCSR && window.document && window.document.documentElement
