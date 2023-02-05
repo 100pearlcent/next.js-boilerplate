2ac680d59df964aa4bc075aa5ff1571420bd22da
100pearlcent <100pearlcent@naver.com>
2023-02-05 12:38:03 +0900

    ⚙️  withMethd

---
+++ src/server/utils/withMethod.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { HttpResponse } from '../interfaces/Http'
+
+export type HttpMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+export type MethodHandler = (req: NextApiRequest, res: NextApiResponse) => void
+export type MethodModel = {
+  [key in HttpMethods]?: MethodHandler
+}
+
+const withMethod =
+  (handlers: MethodModel) =>
+  (req: NextApiRequest, res: NextApiResponse<HttpResponse>) => {
+    const { method } = req
+    if (method in handlers) {
+      const handler = handlers[method]
+      return handler(req, res)
+    }
+    res.setHeader('Allow', Object.keys(handlers))
+    return res.status(405).send({
+      code: 405,
+      message: 'Method not allowed',
+      errors: [`Method "${method}" not allowed.`],
+    })
+  }
+
+export default withMethod
