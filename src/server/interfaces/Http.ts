b144f186af4870f49da84cb7c57f4b9360da323d
100pearlcent <100pearlcent@naver.com>
2023-02-05 12:37:43 +0900

    HTTP interface modified

--- src/server/interfaces/Http.ts
+++ src/server/interfaces/Http.ts
@@ -1,14 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
 export interface HttpResponse<T = unknown, E = string[]> {
   code: number
   message?: string
   payload?: T
   errors?: E
 }
-
-export type HttpMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
-
-export type MethodHandler = (req: NextApiRequest, res: NextApiResponse) => void
-
-export type MethodModel = { [key in HttpMethods]?: MethodHandler }
